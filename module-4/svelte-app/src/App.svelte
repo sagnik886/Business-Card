6a92af3308dfb5ca93a6562c7434ca067c8fa3e8
b219035retr0 <57428197+b219035retr0@users.noreply.github.com>
2023-10-19 06:10:02 +0530

    module-4
    nikita, nanda srikanth and praharsh

---
+++ module-4/svelte-app/src/App.svelte
@@ -0,0 +1,277 @@
+<script>
+	let formData = {
+	  name: "Name",
+	  designation: "Prof",
+	  address: "Address",
+	  phoneNumber: "PhoneNumber",
+	  website: "Website",
+	};
+  
+	let showPrompt = false;
+  
+	let currentPage = "home";
+  
+	const changePage = (page) => {
+	  currentPage = page;
+	};
+  
+	const submitForm = () => {
+	  showPrompt = true;
+	};
+  
+	const closePrompt = () => {
+	  showPrompt = false;
+	};
+  </script>
+  
+  <style>
+	.navbar {
+	  background-color: black;
+	  color: white;
+	  padding: 10px;
+	}
+  
+	.navbar nav {
+	  display: flex;
+	  justify-content: space-between;
+	  align-items: center;
+	}
+  
+	.navbar nav ul {
+	  list-style: none;
+	  padding: 0;
+	  margin: 0;
+	  display: flex;
+	  width: 100%;
+	}
+  
+	.navbar nav li {
+	  flex: 1;
+	  text-align: center;
+	}
+  
+	.navbar nav li a {
+	  text-decoration: none;
+	  color: white;
+	  transition: color 0.3s;
+	}
+  
+	.navbar nav li a:hover {
+	  color: lightgray;
+	}
+  
+	.business-card-container {
+	  display: flex;
+	  justify-content: space-between;
+	}
+  
+	.input-section,
+	.preview-section {
+	  width: 40%;
+	  padding: 20px;
+	  background-color: #f0f0f0;
+	  border: 1px solid #ccc;
+	  border-radius: 5px;
+	}
+  
+	.input-section h3,
+	.preview-section h3 {
+	  font-size: 20px;
+	  margin-bottom: 10px;
+	}
+  
+	.form-group {
+	  margin-bottom: 10px;
+	}
+  
+	.form-group label {
+	  display: block;
+	  margin-bottom: 5px;
+	}
+  
+	.form-group input {
+	  width: 100%;
+	  padding: 10px;
+	  border: 1px solid #ccc;
+	  border-radius: 5px;
+	}
+  
+	.form-group input::placeholder {
+	  color: #aaa;
+	}
+  
+	.button-section {
+	  display: flex;
+	  justify-content: space-between;
+	}
+  
+	.button-section button {
+	  width: 48%;
+	  padding: 10px;
+	  background-color: #007bff;
+	  color: #fff;
+	  border: none;
+	  border-radius: 5px;
+	  cursor: pointer;
+	}
+  
+	.button-section button:hover {
+	  background-color: #0056b3;
+	}
+  
+	.preview-section .business-card-front, .prompt-content .business-card-front
+	 {
+	  width: 300px;
+	  height: 200px;
+	  border: 2px solid #333;
+	  background-color: lightblue;
+	  font-family: 'Times New Roman', Times, serif;
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  justify-content: center;
+	}
+	.preview-section .business-card-back, .prompt-content .business-card-back{
+	  width: 300px;
+	  height: 200px;
+	  border: 2px solid #333;
+	  background-color: lightblue;
+	  font-family: 'Times New Roman', Times, serif;
+	  display: flex;
+	  flex-direction: column;
+	  align-items: left;
+	  justify-content: center;
+	}
+	.preview-section .business-card-front:hover,
+	.preview-section .business-card-back:hover {
+	  background-color: lightcoral;
+	}
+  
+	.business-card-text {
+	  max-width: 80%;
+	}
+  
+	.prompt {
+	  position: fixed;
+	  top: 0;
+	  left: 0;
+	  width: 100%;
+	  height: 100%;
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  background-color: rgba(0, 0, 0, 0.7);
+	}
+  
+	.prompt-content {
+	  background-color: #fff;
+	  padding: 20px;
+	  border-radius: 5px;
+	  text-align: center;
+	}
+  
+	.prompt-content .business-card-front:hover,
+	.prompt-content .business-card-back:hover {
+	  background-color: lightcoral;
+	}
+  </style>
+  
+  <div class="navbar">
+	<nav>
+	  <ul>
+		<li><a href="#" on:click={() => changePage("home")}>Home</a></li>
+		<li><a href="#" on:click={() => changePage("about")}>About</a></li>
+		<li><a href="#" on:click={() => changePage("templates")}>Templates</a></li>
+		<li><a href="#" on:click={() => changePage("support")}>Support</a></li>
+		<li><a href="#" on:click={() => changePage("login")}>Login/Signup</a></li>
+	  </ul>
+	</nav>
+  </div>
+  
+  {#if currentPage === "home"}
+  <div class="business-card-container">
+	<div class="input-section">
+	  <h3>Your Business Card:</h3>
+	  <div class="form-group">
+		<label for="name">Name:</label>
+		<input type="text" id="name" bind:value={formData.name} placeholder="Your Name" />
+	  </div>
+	  <div class="form-group">
+		<label for="designation">Designation:</label>
+		<input type="text" id="designation" bind:value={formData.designation} placeholder="Your Designation" />
+	  </div>
+	  <div class="form-group">
+		<label for="address">Address:</label>
+		<input type="text" id="address" bind:value={formData.address} placeholder="Your Address" />
+	  </div>
+	  <div class="form-group">
+		<label for="phoneNumber">Phone Number:</label>
+		<input type="text" id="phoneNumber" bind:value={formData.phoneNumber} placeholder="Your Phone Number with Country Code" />
+	  </div>
+	  <div class="form-group">
+		<label for="website">Website:</label>
+		<input type="text" id="website" bind:value={formData.website} placeholder="Your Website" />
+	  </div>
+	  <div class="button-section">
+		<button on:click={submitForm}>Submit</button>
+	  </div>
+	</div>
+  
+	<div class="preview-section">
+	  <h3>Front Side:</h3>
+	  <div class="business-card-front">
+		<div class="business-card-text">{formData.name}</div>
+		<div class="business-card-text">{formData.website}</div>
+	  </div>
+	  <h3>Reverse Side:</h3>
+	  <div class="business-card-back">
+		<div class="business-card-text">{formData.name}</div>
+		<div class="business-card-text">{formData.designation}</div>
+		<div class="business-card-text">{formData.address}</div>
+		<div class="business-card-text">{formData.phoneNumber}</div>
+		<div class="business-card-text">{formData.website}</div>
+	  </div>
+	</div>
+  </div>
+  {/if}
+  
+  {#if currentPage === "about"}
+  <div>
+	<!-- About page content goes here -->
+  </div>
+  {/if}
+  
+  {#if currentPage === "templates"}
+  <div>
+	<!-- Templates page content goes here -->
+  </div>
+  {/if}
+  
+  {#if currentPage === "support"}
+  <div>
+	<!-- Support page content goes here -->
+  </div>
+  {/if}
+  
+  {#if showPrompt}
+  <div class="prompt">
+	<div class="prompt-content">
+	  <p>Form Submitted!</p>
+	  <h3>Front Side:</h3>
+	  <div class="business-card-front">
+		<div class="business-card-text">{formData.name}</div>
+		<div class="business-card-text">{formData.website}</div>
+	  </div>
+	  <h3>Reverse Side:</h3>
+	  <div class="business-card-back">
+		<div class="business-card-text">{formData.name}</div>
+		<div class="business-card-text">{formData.designation}</div>
+		<div class="business-card-text">{formData.address}</div>
+		<div class="business-card-text">{formData.phoneNumber}</div>
+		<div class="business-card-text">{formData.website}</div>
+	  </div>
+	  <button on:click={closePrompt}>Close</button>
+	</div>
+  </div>
+  {/if}
+  
